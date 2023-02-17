812b7872fc3a1aae9e6a88ff09a1648d6bac12f1
sheldondebra <54963942+sheldondebra@users.noreply.github.com>
2023-02-17 12:40:05 -0800

    new update

---
+++ else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+int x = 30;
+int y = 20;
+
+if (x>y) {
+    printf("We are good");
+} else { printf("We are bad");
+}
+    return(0);
+}
--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-int age = 5;
-printf("%d", age);
-
-    return(0);
-}
